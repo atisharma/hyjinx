dbff4ad0158db149be5c86ad12e28dfe762ab0d5
Ati Sharma <ati@agalmic.ltd>
2024-05-04 20:55:31 +0100

    version bump

--- hyjinx/__init__.py
+++ hyjinx/__init__.py
@@ -37,5 +37,5 @@ except ModuleNotFoundError:
 hy.macros.require('hyjinx.macros', None, assignments = 'ALL', prefix = '')
 
 # set the package version
-__version__ = "0.28.24"
+__version__ = "0.28.25"
 __version_info__ = __version__.split(".")
