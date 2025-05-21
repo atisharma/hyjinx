f39131f9ac98ab5a27f4ed684c3fc1d41293f22b
Ati Sharma <dev@agalmic.ltd>
2025-05-21 13:05:23 +0100

    version bump

--- hyjinx/__init__.py
+++ hyjinx/__init__.py
@@ -42,5 +42,5 @@ hy.macros.require('hyjinx.macros', None, assignments = 'ALL', prefix = '')
 
 # set the package version
 # the major.minor version simply match the assumed Hy version
-__version__ = "1.0.11"
+__version__ = "1.0.13"
 __version_info__ = __version__.split(".")
